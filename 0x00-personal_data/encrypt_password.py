4213a7787b2fbf7051a585bfcbf1d74de88e349d
math198512 <thoraichi@gmail.com>
2024-11-09 19:46:17 +0100

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,6 +5,10 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Hashes a password using a random salt.
-    """
+    """Hashes a password using a random salt."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check if the hashed password matches the real password"""
+    return bcrypt.checkpw(password, hashed_password)

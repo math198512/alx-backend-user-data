b73513cf75d490114ba8f42690b5cc36158a1d33
math198512 <thoraichi@gmail.com>
2024-11-12 20:01:10 +0100

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,6 +7,8 @@ from typing import List, TypeVar
 
 
 user_type = TypeVar('User')
+
+
 class Auth():
     """Authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:

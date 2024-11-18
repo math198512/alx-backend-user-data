cebbfc801aeddef42c4a0a26d511648e22b97e76
math198512 <thoraichi@gmail.com>
2024-11-18 09:49:52 +0100

    basic_auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Basic authentication module for the API.
+"""
+from typing import List
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class.
+    """
+    pass

26424f046423ff1582d6d46d2dfc2d95a008b987
math198512 <thoraichi@gmail.com>
2024-11-19 20:17:11 +0100

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -60,6 +60,7 @@ def authenticate_user():
                 abort(403)
             request.current_user = auth.current_user(request)
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")

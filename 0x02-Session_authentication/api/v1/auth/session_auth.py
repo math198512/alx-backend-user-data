fecfdb912d98e82406ca83bd12946158135b7c6c
math198512 <thoraichi@gmail.com>
2024-11-20 14:09:05 +0100

    session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,4 +30,4 @@ class SessionAuth(Auth):
         """returns a User ID based on a Session ID"""
         if session_id is None or type(session_id) != str:
             return None
-        return self.user_id_by_session_id.get('session_id')
+        return self.user_id_by_session_id.get(session_id)

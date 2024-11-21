489d2a73168ebf09db92139ce764696d883998a4
math198512 <thoraichi@gmail.com>
2024-11-21 21:49:26 +0100

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,9 +63,9 @@ def authenticate_user():
             if auth.current_user(request) is None:
                 abort(403)
             request.current_user = auth.current_user(request)
-        if auth.authorization_header(request) is None and \
-                auth.session_cookie(request) is None:
-            abort(401)
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
+                abort(401)
 
 
 if __name__ == "__main__":

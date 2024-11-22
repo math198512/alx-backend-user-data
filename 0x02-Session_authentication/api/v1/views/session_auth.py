6f9e5dfec05fc1ea507188c388d548f785e8be55
math198512 <thoraichi@gmail.com>
2024-11-22 18:10:22 +0100

    session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -35,10 +35,12 @@ def login():
     response.set_cookie(getenv('SESSION_NAME'), session_id)
     return response
 
-# @app_views.route('/auth_session/logout',
-# methods=['DELETE'], strict_slashes=False)
-# def logout():
-#     """Handles user logout and deletes a session"""
-#     if not auth.destroy_session(request):
-#         abort(404)
-#     return jsonify({}), 200
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Handles user logout and deletes a session"""
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200

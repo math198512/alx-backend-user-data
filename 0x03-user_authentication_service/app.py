a566cd17dfa8e1a0a3a0e6a9a293de16940e5799
math198512 <thoraichi@gmail.com>
2024-11-24 20:02:05 +0100

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,9 +5,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

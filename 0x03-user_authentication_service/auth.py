9a92ad1f07bdafae2084177a1f8e280111a178b8
math198512 <thoraichi@gmail.com>
2024-11-24 18:07:36 +0100

    auth.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+User Authentication Service
+"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+    Args:
+        password (str): The password to hash.
+    Returns:
+        bytes: The hashed password.
+    """
+    return bcrypt.hashpw(password)
